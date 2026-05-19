04b399d7f613cd9956dbd42fca60689e2421922f
angkharin <angsae20@gmail.com>
2026-05-19 15:24:08 +0700

    update

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")));
+
+
+app.set("view","./src/view");
+app.set("view engine","ejs")
 
 app.get("/" ,(req,res) =>{
 
-    res.send('Hello Ang');
+    res.render('index',{username: 'Ang55+',customers: ["Kitti112","Kittikorn","Kitty"]});
 })
 
 
 app.listen(port, ()=>{
-    debug("Listening on port" + chalk.green(" : "+port));
+    debug("Listening on port" + chalk.red(" : "+port));
 })
